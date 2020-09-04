3e9a6d344c10df89e9c70726a41c1ac4fddf1e53
Cedric Johnson <cedric@unaffiliatedstudios.com>
2020-09-04 12:38:35 -0500

    Card component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./App.css";
+import { CardList } from './components/card-list/card-list.component'
 
 class App extends Component {
   constructor() {
@@ -19,10 +20,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          {
+          <CardList name='Fireside'>
+           {
             this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>
               )
           }
+          </CardList>
       </div>
     );
   }
