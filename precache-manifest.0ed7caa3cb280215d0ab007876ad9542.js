72409d5f65c97b97cb3e18e9abd88b4ce8849c08
Cedric Johnson <cedric@unaffiliatedstudios.com>
2020-09-04 20:37:48 -0500

    Updates

---
+++ precache-manifest.0ed7caa3cb280215d0ab007876ad9542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982cd18f93b044ca34b8e1918afe573b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9d9f8dc8433637a2de23",
+    "url": "/monsters-rolodex/static/css/main.b80164f4.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d9f8dc8433637a2de23",
+    "url": "/monsters-rolodex/static/js/main.69cef928.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
